7dfe0f7ef58de1a0f078c45966c66144141a5007
Ioniized <100499056+Ioniized@users.noreply.github.com>
2022-04-04 15:54:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var desc = ["Mercury is the first planet from the sun. It is the smallest planet
             ]
             var i = 0;
             function next(){
-                if(i > desc[i].length + 1){
+                if(i > 13){
                     i = 0;
                 }
                 document.getElementById("description").innerHTML = desc[i]
